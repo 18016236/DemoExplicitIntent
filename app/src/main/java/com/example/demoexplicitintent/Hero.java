6ccbf73315258e48018d24d4a2f62329bf072cb9
18016236 <18016236@myrp.edu.sg>
2020-05-08 10:37:09 +0800

    commits

---
+++ app/src/main/java/com/example/demoexplicitintent/Hero.java
@@ -0,0 +1,39 @@
+package com.example.demoexplicitintent;
+
+import java.io.Serializable;
+
+public class Hero implements Serializable {
+    String name;
+    int strength;
+    int technicalProwess;
+
+    public Hero(String name, int strength, int technicalProwess) {
+        this.name = name;
+        this.strength = strength;
+        this.technicalProwess = technicalProwess;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getTechnicalProwess() {
+        return technicalProwess;
+    }
+
+    public void setTechnicalProwess(int technicalProwess) {
+        this.technicalProwess = technicalProwess;
+    }
+}
